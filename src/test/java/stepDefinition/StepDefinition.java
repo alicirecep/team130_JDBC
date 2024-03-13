fac00beeecfe7ed9d594e0f080e951a98774d5b3
Recep ALICI <alicirecepp@gmail.com>
2024-03-13 20:20:03 +0100

    Recep/13/03/2024/V1

--- src/test/java/stepDefinition/StepDefinition.java
+++ src/test/java/stepDefinition/StepDefinition.java
@@ -1,16 +1,19 @@
 package stepDefinition;
 
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
 import utilities.JDBCReusableMethods;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.junit.Assert.assertEquals;
+
 public class StepDefinition {
 
     ResultSet resultSet;
 
-
+/*
     @Given("Database baglantisi kurulur.")
     public void database_baglantisi_kurulur() {
 
@@ -26,15 +29,49 @@ public class StepDefinition {
     }
 
     @Given("ResultSet01 sonuclari islenir.")
-    public void result_set01_sonuclari_islenir() {
+    public void result_set01_sonuclari_islenir() throws SQLException {
 
+        resultSet.next();
+        int actualResult = resultSet.getInt("user_id");
+        int expectedResult = 1;
+
+        assertEquals(expectedResult,actualResult);
 
     }
 
     @Given("Database baglantisi kapatilir.")
     public void database_baglantisi_kapatilir() {
 
+        JDBCReusableMethods.closeConnection();
 
+    }
+
+*/
+
+    @Given("Database baglantisi kurulur.")
+    public void database_baglantisi_kurulur() {
+        JDBCReusableMethods.getConnection();
+    }
+
+    @Given("Query01 hazirlanir ve execute edilir.")
+    public void query01_hazirlanir_ve_execute_edilir() throws SQLException {
+        String query01 = "SELECT user_id FROM u168183796_qaloantec.deposits WHERE amount BETWEEN 100 AND 500";
+        resultSet = JDBCReusableMethods.getStatement().executeQuery(query01);
+    }
+
+    @Given("ResultSet01 sonuclari islenir.")
+    public void result_set01_sonuclari_islenir() throws SQLException {
+        resultSet.next();
+        int actualResult = resultSet.getInt("user_id");
+        System.out.println(actualResult);
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Given("Database baglantisi kapatilir.")
+    public void database_baglantisi_kapatilir() {
+        JDBCReusableMethods.closeConnection();
     }
 
 
--- src/test/resources/select.feature
+++ src/test/resources/select.feature
@@ -1,6 +1,6 @@
 Feature: Select Query Execute
 
-
+  @query01
   Scenario: Database içindeki "deposits" toblosunda "amount" değeri
             100$ ile 500$ arasında olan user_id’leri doğrulayınız.
 
